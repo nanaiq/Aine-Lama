b5df85b78b5ccc3c00fdebda9231aa2ae5a4f801
nanaiq <108739585+nanaiq@users.noreply.github.com>
2022-07-09 20:05:36 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [085842769268]
+│ •  [081227250948]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
