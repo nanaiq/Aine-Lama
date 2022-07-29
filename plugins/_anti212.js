9b13d08d201182ec8675637a0a0addbb358a4c28
nanaiq <108739585+nanaiq@users.noreply.github.com>
2022-07-29 18:45:35 +0700

    Update _anti212.js

--- plugins/_anti212.js
+++ plugins/_anti212.js
@@ -3,11 +3,19 @@ let handler = m => m
 handler.before = async function (m) {
    if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = true
+      global.db.data.users[m.sender].kick = true
     await conn.updateBlockStatus(m.sender, "block") // hapus kalau ga mau pasang
    }
    
    if (m.sender.startsWith('92' || '92')) {
+      global.db.data.users[m.sender].banned = true
+      global.db.data.users[m.sender].kick = true
+    await conn.updateBlockStatus(m.sender, "block") // hapus kalau ga mau pasang
+   }
+   
+   if (m.sender.startsWith('91' || '91')) {
    	global.db.data.users[m.sender].banned = true
+      global.db.data.users[m.sender].kick = true
     await conn.updateBlockStatus(m.sender, "block") // hapus kalau ga mau pasang
    }
  }
